43b2be39012215fe37fb0fd2b25f87f781569c02
MartijnGrobbink <48592206+MartijnGrobbink@users.noreply.github.com>
2022-05-17 13:10:27 +0200

    cossing script added

---
+++ Totally  Accurate  Gladiator  Arena Game Files/Assets/Scripts/Crossing.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crossing : MonoBehaviour
+{
+    public Transform North;
+    public Transform South;
+    public Transform East;
+    public Transform West;
+}
