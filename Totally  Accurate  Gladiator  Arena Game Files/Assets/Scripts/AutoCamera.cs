cf78ce6fc727c5882b46ee22a24e3d772d1c63bd
Daniel Lopes <danny04lopes@gmail.com>
2022-05-25 22:09:19 +0200

    Camera script set up
    Testing movement from coord A to B, working on the remaining of it, need to commit first

---
+++ Totally  Accurate  Gladiator  Arena Game Files/Assets/Scripts/AutoCamera.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoCamera : MonoBehaviour
+{
+    //camera and sectors
+    public GameObject camera01;
+    public GameObject sector1;
+    public GameObject sector2;
+    public GameObject sector3;
+    public GameObject sector4;
+
+    private int sector1Points = 0;
+    private int sector2Points = 0;
+    private int sector3Points = 0;
+    private int sector4Points = 0;
+
+    public Transform Sec1Cam;
+    public float t;
+    public float speed;
+
+    //sector array
+    private GameObject[] sectors;
+
+    //lista ou array com as varias posicoes de camera para os varios sectors
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //every2 seconds remove 1 points
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //every 10 secs check change sector
+        Vector3 a = transform.position;
+        Vector3 b = Sec1Cam.position;
+        transform.position = Vector3.MoveTowards(a, Vector3.Lerp(a,b,t), speed);
+
+    }
+
+    public void AddPoints()
+    {
+        //if statue inside zone and being contested give sector 1 points + 1 point per second while being contested
+        //if AI took a hit/damage give sector 1 point
+    }
+
+    public void moveCamera(Vector3 position)
+    {
+        //move camera to different sector positions?
+        this.transform.position = position;
+        
+    }
+
+    public void ChangeSector()
+    {
+        //check if there is a sector with higher points than the current one
+        //if higher and only higher change current sector to that one
+        //moveCamera to that sector coords
+    }
+
+
+}
