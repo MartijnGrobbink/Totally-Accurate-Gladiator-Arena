4b7e1030068b4934dfadff5526b38fddc4024bba
Daniel Lopes <danny04lopes@gmail.com>
2022-05-31 14:02:54 +0200

    Auto camera update part3
    Detects tags in collision, if tag = statue and being contest, whilst being contested sector receives points
    If the tag is something else that not statue, assumes AI's tags and if they receive damage sector gets points
    theoretically it's working, needs some tests on gameplay scene after states settled

--- Totally  Accurate  Gladiator  Arena Game Files/Assets/Scripts/Health/HealthSystem.cs
+++ Totally  Accurate  Gladiator  Arena Game Files/Assets/Scripts/Health/HealthSystem.cs
@@ -6,6 +6,8 @@ public class HealthSystem {
     private int health;
     private int healthMax;
 
+    private int damaged = 0;
+
     public HealthSystem(int healthMax)
     {
         this.healthMax = healthMax;
@@ -16,16 +18,28 @@ public class HealthSystem {
     public int GetHealth() {
         return health;
     }
+
     public float GetHealthPercent()
     {
         return (float)health / healthMax;
     }
+
+    //-----------------------For the Camera-----------------------------------
+    public int getDamaged()
+    {
+        return damaged;
+    }
+
+    //------------------------------------------------------------------------
 
     public void Damage(int damageAmount)
     {
         health -= damageAmount;
         if (health < 0) health = 0;
         if (OnHealthChanged != null) OnHealthChanged(this, EventArgs.Empty);
+
+        // to track the number of times an AI received damage on a certain sector 
+        damaged++;
     }
 
     public void Heal(int healAmount)
