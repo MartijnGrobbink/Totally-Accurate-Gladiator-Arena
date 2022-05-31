72346ac2c14064cb4ef92b1b21ec5b337faab876
Daniel Lopes <danny04lopes@gmail.com>
2022-05-31 16:04:59 +0200

    AOE update
    more set up, almost done need some changes

--- Totally  Accurate  Gladiator  Arena Game Files/Assets/Scripts/Weapons/AOE.cs
+++ Totally  Accurate  Gladiator  Arena Game Files/Assets/Scripts/Weapons/AOE.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class AOE : MonoBehaviour
 {
     //---------------------------------------------------Variables---------------------------------------------------------------
+    private WeaponStats weapons;
+    private bool DealDamage;
+    private bool Attacked;
+    public GameObject EnemyAttacked;
+
 
     //----------------------------------------------Start and Update-------------------------------------------------------------
     void Start()
@@ -21,9 +26,45 @@ public class AOE : MonoBehaviour
     //---------------------------------------------------------------------------------------------------------------------------
     public void OnTriggerEnter(Collider other)
     {
-        //check if is not same tag
+        Debug.Log("entered collision");
+        if (DealDamage == false && Attacked == true)
+        {
+            var x = gameObject.transform.parent;
+            if (x.gameObject.tag != other.gameObject.tag)
+            {
+                EnemyAttacked = other.gameObject;
+
+                DealDamage = true;
+            }
+        
+        }  
         //if can attack == true & in range of AOE 
             //call attack as well for them but only deal damage (private serielizedfield)
             //call damage value from weaponstats for the 
     }
+
+    private void CheckInRange()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+        for (int i = 0; i < rangeChecks.Length; i++)
+        {
+            if (inRange.Contains(rangeChecks[i].gameObject) != true && rangeChecks[i].gameObject != gameObject && rangeChecks[i].tag != "Weapon")
+                inRange.Add(rangeChecks[i].gameObject);
+        }
+        CheckIfStillInRange();
+    }
+
+    private void CheckIfStillInRange()
+    {
+        for (int i = 0; i < inRange.Count; i++)
+        {
+            float dist = (gameObject.transform.position - inRange[i].transform.position).magnitude;
+            if (dist > radius)
+            {
+                inRange.Remove(inRange[i].gameObject);
+            }
+        }
+    }
 }
+
+
