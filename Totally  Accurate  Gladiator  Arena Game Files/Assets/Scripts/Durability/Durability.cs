e1bdd0e0b39009863aec0ce71fcaea09b888ca93
Rui Lima <r7lima3@hotmail.com>
2022-06-03 12:18:37 +0100

    Spawning + Durability
    Added the Spawning for the characters and added the durability script.

---
+++ Totally  Accurate  Gladiator  Arena Game Files/Assets/Scripts/Durability/Durability.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Durability : MonoBehaviour
+{
+    public float maxDurability = 30.0f;  // max durability
+    public float curDurability;   // current durability
+
+    void Start()
+    {
+        curDurability = maxDurability;  
+    }
+
+    public void HitSomething()
+    {
+        curDurability -= 5; 
+
+        if (curDurability <= 0)
+            Destroy(gameObject);
+        {
+            Debug.Log(gameObject.name + " is broken!"); 
+        }
+    }
+}
