d73ac263f12c14680ed297589dcdea8b324a69ea
StatisticallyMeh <statisticallymeh@gmail.com>
2022-06-06 13:56:09 +0200

    UI Version 2 Fully Functional
    Each part of the UI works now using the functions set up in each script that is held for each part of the UI. It is testable using the testing gameobject in the UIV2 hierarchy

--- Totally  Accurate  Gladiator  Arena Game Files/Assets/Scripts/UI2.0/UI_SetUp.cs
+++ Totally  Accurate  Gladiator  Arena Game Files/Assets/Scripts/UI2.0/UI_SetUp.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class UI_SetUp : MonoBehaviour
 {
     public UI_Refs UR;
+    public UI_Score US;
+
     void Start()
     {
         //Setting Up Members UI
@@ -17,6 +19,28 @@ public class UI_SetUp : MonoBehaviour
         UR.TeamMembers.Add("Leader", Leader);
         UR.TeamMembers.Add("MemberB", MemberB);
 
+        var aaa = UR.TeamMembers["MemberA"];
+        var weaponaaa = aaa.transform.Find("TeamMemberWeaponImage");
+
+        var tempColor = weaponaaa.GetComponent<Image>().color;
+        tempColor.a = 0f;
+        weaponaaa.GetComponent<Image>().color = tempColor;
+
+        var bbb = UR.TeamMembers["Leader"];
+        var weaponbbb = bbb.transform.Find("TeamMemberWeaponImage");
+
+        tempColor = weaponbbb.GetComponent<Image>().color;
+        tempColor.a = 0f;
+        weaponbbb.GetComponent<Image>().color = tempColor;
+
+        var ccc = UR.TeamMembers["MemberB"];
+        var weaponccc = ccc.transform.Find("TeamMemberWeaponImage");
+
+        tempColor = weaponccc.GetComponent<Image>().color;
+        tempColor.a = 0f;
+        weaponccc.GetComponent<Image>().color = tempColor;
+
+        //Setting Up Teams UI
         var TeamA = Instantiate(UR.TeamsKilledInfoPrefab, new Vector3(0, 0, 0), Quaternion.identity, UR.TeamsKilledInfoLayout.transform);
         var TeamB = Instantiate(UR.TeamsKilledInfoPrefab, new Vector3(0, 0, 0), Quaternion.identity, UR.TeamsKilledInfoLayout.transform);
         var TeamC = Instantiate(UR.TeamsKilledInfoPrefab, new Vector3(0, 0, 0), Quaternion.identity, UR.TeamsKilledInfoLayout.transform);
@@ -38,7 +62,7 @@ public class UI_SetUp : MonoBehaviour
         var y = UR.WeaponsUsed["WeaponA"];
         var weapona = y.transform.Find("UsedWeapon");
 
-        var tempColor = weapona.GetComponent<Image>().color;
+        tempColor = weapona.GetComponent<Image>().color;
         tempColor.a = 0f;
         weapona.GetComponent<Image>().color = tempColor;
 
@@ -55,5 +79,69 @@ public class UI_SetUp : MonoBehaviour
         tempColor = weaponc.GetComponent<Image>().color;
         tempColor.a = 0f;
         weaponc.GetComponent<Image>().color = tempColor;
+
+        //Setting Up Kill Feed UI
+        var KillA = Instantiate(UR.KillLogPrefab, new Vector3(0, 0, 0), Quaternion.identity, UR.KillLogLayout.transform);
+        var KillB = Instantiate(UR.KillLogPrefab, new Vector3(0, 0, 0), Quaternion.identity, UR.KillLogLayout.transform);
+        var KillC = Instantiate(UR.KillLogPrefab, new Vector3(0, 0, 0), Quaternion.identity, UR.KillLogLayout.transform);
+        var KillD = Instantiate(UR.KillLogPrefab, new Vector3(0, 0, 0), Quaternion.identity, UR.KillLogLayout.transform);
+
+        UR.KillLogs.Add("KillD", KillA);
+        UR.KillLogs.Add("KillC", KillB);
+        UR.KillLogs.Add("KillB", KillC);
+        UR.KillLogs.Add("KillA", KillD);
+
+        var a = UR.KillLogs["KillA"];
+        var killa = a.transform.Find("Weapon");
+
+        tempColor = killa.GetComponent<Image>().color;
+        tempColor.a = 0f;
+        killa.GetComponent<Image>().color = tempColor;
+
+        var b = UR.KillLogs["KillB"];
+        var killb = b.transform.Find("Weapon");
+
+        tempColor = killb.GetComponent<Image>().color;
+        tempColor.a = 0f;
+        killb.GetComponent<Image>().color = tempColor;
+
+        var c = UR.KillLogs["KillC"];
+        var killc = c.transform.Find("Weapon");
+
+        tempColor = killc.GetComponent<Image>().color;
+        tempColor.a = 0f;
+        killc.GetComponent<Image>().color = tempColor;
+
+        var d = UR.KillLogs["KillD"];
+        var killd = d.transform.Find("Weapon");
+
+        tempColor = killd.GetComponent<Image>().color;
+        tempColor.a = 0f;
+        killd.GetComponent<Image>().color = tempColor;
+        //Setting Up Score UI
+        var ScoreA = Instantiate(UR.ScorePrefab, new Vector3(0, 0, 0), Quaternion.identity, UR.ScoreLayout.transform);
+        var ScoreB = Instantiate(UR.ScorePrefab, new Vector3(0, 0, 0), Quaternion.identity, UR.ScoreLayout.transform);
+        var ScoreC = Instantiate(UR.ScorePrefab, new Vector3(0, 0, 0), Quaternion.identity, UR.ScoreLayout.transform);
+        var ScoreD = Instantiate(UR.ScorePrefab, new Vector3(0, 0, 0), Quaternion.identity, UR.ScoreLayout.transform);
+        var ScoreE = Instantiate(UR.ScorePrefab, new Vector3(0, 0, 0), Quaternion.identity, UR.ScoreLayout.transform);
+
+        UR.Scores.Add("Cavemen", ScoreA);
+        UR.Scores.Add("Vikings", ScoreB);
+        UR.Scores.Add("Romans", ScoreC);
+        UR.Scores.Add("Knights", ScoreD);
+        UR.Scores.Add("Gamers", ScoreE);
+
+        US.SetTeamLogos();
+        //Setting up Statue
+        var StatueA = Instantiate(UR.StatuePrefab, new Vector3(0, 0, 0), Quaternion.identity, UR.StatueLayout.transform);
+
+        UR.Statues.Add("StatueA", StatueA);
+
+        var aa = UR.Statues["StatueA"];
+        var statueaa = aa.transform.Find("TeamImageCap");
+
+        tempColor = statueaa.GetComponent<Image>().color;
+        tempColor.a = 0f;
+        statueaa.GetComponent<Image>().color = tempColor;
     }
 }
---
+++ Totally  Accurate  Gladiator  Arena Game Files/Assets/Scripts/UI2.0/UI_Statue.cs
@@ -0,0 +1,183 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class UI_Statue : MonoBehaviour
+{
+    public UI_Refs UR;
+    public Teams_Data TD;
+    public Team_Assets TA;
+    public Statue_Assets SA;
+
+    public int CapturedPoints;
+    public int KilledPoints;
+
+    void Start()
+    {
+
+    }
+
+    public void TeamsStatueStatus(string team)
+    {
+        CheckActiveTeam(team);
+    }
+
+    private void CheckActiveTeam(string team)
+    {
+        if (team == "NoTeam")
+        {
+            var a = UR.Statues["StatueA"];
+            var statuea = a.transform.Find("StatueStatus");
+            var statuecapa = a.transform.Find("TeamImageCap");
+
+            foreach (Sprite sprite in SA.StatueImages)
+            {
+                if (sprite.name == "ContestedStatue")
+                {
+                    statuea.GetComponent<Image>().sprite = sprite;
+                }
+            }
+
+            var tempColor = statuecapa.GetComponent<Image>().color;
+            tempColor.a = 0f;
+            statuecapa.GetComponent<Image>().color = tempColor;
+        }
+
+        if (team == "Cavemen")
+        {
+            var a = UR.Statues["StatueA"];
+            var statuea = a.transform.Find("StatueStatus");
+            var statuecapa = a.transform.Find("TeamImageCap");
+
+            foreach (Sprite sprite in SA.StatueImages)
+            {
+                if (sprite.name == "CavemanStatue")
+                {
+                    statuea.GetComponent<Image>().sprite = sprite;
+                }
+            }
+
+            foreach (Sprite sprite in TA.CharactersImages)
+            {
+                if (sprite.name == "cavemanleader")
+                {
+                    statuecapa.GetComponent<Image>().sprite = sprite;
+                }
+            }
+
+            var tempColor = statuecapa.GetComponent<Image>().color;
+            tempColor.a = 1f;
+            statuecapa.GetComponent<Image>().color = tempColor;
+        }
+
+        if (team == "Gamers")
+        {
+            var a = UR.Statues["StatueA"];
+            var statuea = a.transform.Find("StatueStatus");
+            var statuecapa = a.transform.Find("TeamImageCap");
+
+            foreach (Sprite sprite in SA.StatueImages)
+            {
+                if (sprite.name == "GamerStatue")
+                {
+                    statuea.GetComponent<Image>().sprite = sprite;
+                }
+            }
+
+            foreach (Sprite sprite in TA.CharactersImages)
+            {
+                if (sprite.name == "gamerleader")
+                {
+                    statuecapa.GetComponent<Image>().sprite = sprite;
+                }
+            }
+
+            var tempColor = statuecapa.GetComponent<Image>().color;
+            tempColor.a = 1f;
+            statuecapa.GetComponent<Image>().color = tempColor;
+        }
+
+        if (team == "Vikings")
+        {
+            var a = UR.Statues["StatueA"];
+            var statuea = a.transform.Find("StatueStatus");
+            var statuecapa = a.transform.Find("TeamImageCap");
+
+            foreach (Sprite sprite in SA.StatueImages)
+            {
+                if (sprite.name == "VikingStatue")
+                {
+                    statuea.GetComponent<Image>().sprite = sprite;
+                }
+            }
+
+            foreach (Sprite sprite in TA.CharactersImages)
+            {
+                if (sprite.name == "vikingleader")
+                {
+                    statuecapa.GetComponent<Image>().sprite = sprite;
+                }
+            }
+
+            var tempColor = statuecapa.GetComponent<Image>().color;
+            tempColor.a = 1f;
+            statuecapa.GetComponent<Image>().color = tempColor;
+        }
+
+        if (team == "Knights")
+        {
+            var a = UR.Statues["StatueA"];
+            var statuea = a.transform.Find("StatueStatus");
+            var statuecapa = a.transform.Find("TeamImageCap");
+
+            foreach (Sprite sprite in SA.StatueImages)
+            {
+                if (sprite.name == "KnightStatue")
+                {
+                    statuea.GetComponent<Image>().sprite = sprite;
+                }
+            }
+
+            foreach (Sprite sprite in TA.CharactersImages)
+            {
+                if (sprite.name == "knightleader")
+                {
+                    statuecapa.GetComponent<Image>().sprite = sprite;
+                }
+            }
+
+            var tempColor = statuecapa.GetComponent<Image>().color;
+            tempColor.a = 1f;
+            statuecapa.GetComponent<Image>().color = tempColor;
+        }
+
+        if (team == "Romans")
+        {
+            var a = UR.Statues["StatueA"];
+            var statuea = a.transform.Find("StatueStatus");
+            var statuecapa = a.transform.Find("TeamImageCap");
+
+            foreach (Sprite sprite in SA.StatueImages)
+            {
+                if (sprite.name == "RomanStatue")
+                {
+                    statuea.GetComponent<Image>().sprite = sprite;
+                }
+            }
+
+            foreach (Sprite sprite in TA.CharactersImages)
+            {
+                if (sprite.name == "romanleader")
+                {
+                    statuecapa.GetComponent<Image>().sprite = sprite;
+                }
+            }
+
+            var tempColor = statuecapa.GetComponent<Image>().color;
+            tempColor.a = 1f;
+            statuecapa.GetComponent<Image>().color = tempColor;
+        }
+    }
+}
