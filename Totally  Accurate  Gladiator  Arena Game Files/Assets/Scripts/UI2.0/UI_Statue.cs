28180d22e783903aa36fbf955a0fbb18f017370b
StatisticallyMeh <statisticallymeh@gmail.com>
2022-06-13 15:01:49 +0200

    Fixed UI Statue
    UI Statue was still calling in end scene now it will no longer find null refs

--- Totally  Accurate  Gladiator  Arena Game Files/Assets/Scripts/UI2.0/UI_Statue.cs
+++ Totally  Accurate  Gladiator  Arena Game Files/Assets/Scripts/UI2.0/UI_Statue.cs
@@ -26,178 +26,182 @@ public class UI_Statue : MonoBehaviour
 
     private void CheckActiveTeam(string team)
     {
-        if (team == "NoTeam")
+        if (UR.Statues["StatueA"])
         {
-            var a = UR.Statues["StatueA"];
-            var statuea = a.transform.Find("StatueStatus");
-            var statuecapa = a.transform.Find("TeamImageCap");
-
-            foreach (Sprite sprite in SA.StatueImages)
+            if (team == "NoTeam")
             {
-                if (sprite.name == "NoTeamStatue")
+                var a = UR.Statues["StatueA"];
+                var statuea = a.transform.Find("StatueStatus");
+                var statuecapa = a.transform.Find("TeamImageCap");
+
+                foreach (Sprite sprite in SA.StatueImages)
                 {
-                    statuea.GetComponent<Image>().sprite = sprite;
+                    if (sprite.name == "NoTeamStatue")
+                    {
+                        statuea.GetComponent<Image>().sprite = sprite;
+                    }
                 }
+
+                var tempColor = statuecapa.GetComponent<Image>().color;
+                tempColor.a = 0f;
+                statuecapa.GetComponent<Image>().color = tempColor;
             }
 
-            var tempColor = statuecapa.GetComponent<Image>().color;
-            tempColor.a = 0f;
-            statuecapa.GetComponent<Image>().color = tempColor;
+            if (team == "Contested")
+            {
+                var a = UR.Statues["StatueA"];
+                var statuea = a.transform.Find("StatueStatus");
+                var statuecapa = a.transform.Find("TeamImageCap");
+
+                foreach (Sprite sprite in SA.StatueImages)
+                {
+                    if (sprite.name == "ContestedStatue")
+                    {
+                        statuea.GetComponent<Image>().sprite = sprite;
+                    }
+                }
+
+                var tempColor = statuecapa.GetComponent<Image>().color;
+                tempColor.a = 0f;
+                statuecapa.GetComponent<Image>().color = tempColor;
+            }
+
+            if (team == "Cavemen")
+            {
+                var a = UR.Statues["StatueA"];
+                var statuea = a.transform.Find("StatueStatus");
+                var statuecapa = a.transform.Find("TeamImageCap");
+
+                foreach (Sprite sprite in SA.StatueImages)
+                {
+                    if (sprite.name == "CavemanStatue")
+                    {
+                        statuea.GetComponent<Image>().sprite = sprite;
+                    }
+                }
+
+                foreach (Sprite sprite in TA.CharactersImages)
+                {
+                    if (sprite.name == "cavemanleader")
+                    {
+                        statuecapa.GetComponent<Image>().sprite = sprite;
+                    }
+                }
+
+                var tempColor = statuecapa.GetComponent<Image>().color;
+                tempColor.a = 1f;
+                statuecapa.GetComponent<Image>().color = tempColor;
+            }
+
+            if (team == "Gamers")
+            {
+                var a = UR.Statues["StatueA"];
+                var statuea = a.transform.Find("StatueStatus");
+                var statuecapa = a.transform.Find("TeamImageCap");
+
+                foreach (Sprite sprite in SA.StatueImages)
+                {
+                    if (sprite.name == "GamerStatue")
+                    {
+                        statuea.GetComponent<Image>().sprite = sprite;
+                    }
+                }
+
+                foreach (Sprite sprite in TA.CharactersImages)
+                {
+                    if (sprite.name == "gamerleader")
+                    {
+                        statuecapa.GetComponent<Image>().sprite = sprite;
+                    }
+                }
+
+                var tempColor = statuecapa.GetComponent<Image>().color;
+                tempColor.a = 1f;
+                statuecapa.GetComponent<Image>().color = tempColor;
+            }
+
+            if (team == "Vikings")
+            {
+                var a = UR.Statues["StatueA"];
+                var statuea = a.transform.Find("StatueStatus");
+                var statuecapa = a.transform.Find("TeamImageCap");
+
+                foreach (Sprite sprite in SA.StatueImages)
+                {
+                    if (sprite.name == "VikingStatue")
+                    {
+                        statuea.GetComponent<Image>().sprite = sprite;
+                    }
+                }
+
+                foreach (Sprite sprite in TA.CharactersImages)
+                {
+                    if (sprite.name == "vikingleader")
+                    {
+                        statuecapa.GetComponent<Image>().sprite = sprite;
+                    }
+                }
+
+                var tempColor = statuecapa.GetComponent<Image>().color;
+                tempColor.a = 1f;
+                statuecapa.GetComponent<Image>().color = tempColor;
+            }
+
+            if (team == "Knights")
+            {
+                var a = UR.Statues["StatueA"];
+                var statuea = a.transform.Find("StatueStatus");
+                var statuecapa = a.transform.Find("TeamImageCap");
+
+                foreach (Sprite sprite in SA.StatueImages)
+                {
+                    if (sprite.name == "KnightStatue")
+                    {
+                        statuea.GetComponent<Image>().sprite = sprite;
+                    }
+                }
+
+                foreach (Sprite sprite in TA.CharactersImages)
+                {
+                    if (sprite.name == "knightleader")
+                    {
+                        statuecapa.GetComponent<Image>().sprite = sprite;
+                    }
+                }
+
+                var tempColor = statuecapa.GetComponent<Image>().color;
+                tempColor.a = 1f;
+                statuecapa.GetComponent<Image>().color = tempColor;
+            }
+
+            if (team == "Romans")
+            {
+                var a = UR.Statues["StatueA"];
+                var statuea = a.transform.Find("StatueStatus");
+                var statuecapa = a.transform.Find("TeamImageCap");
+
+                foreach (Sprite sprite in SA.StatueImages)
+                {
+                    if (sprite.name == "RomanStatue")
+                    {
+                        statuea.GetComponent<Image>().sprite = sprite;
+                    }
+                }
+
+                foreach (Sprite sprite in TA.CharactersImages)
+                {
+                    if (sprite.name == "romanleader")
+                    {
+                        statuecapa.GetComponent<Image>().sprite = sprite;
+                    }
+                }
+
+                var tempColor = statuecapa.GetComponent<Image>().color;
+                tempColor.a = 1f;
+                statuecapa.GetComponent<Image>().color = tempColor;
+            }
         }
-
-        if (team == "Contested")
-        {
-            var a = UR.Statues["StatueA"];
-            var statuea = a.transform.Find("StatueStatus");
-            var statuecapa = a.transform.Find("TeamImageCap");
-
-            foreach (Sprite sprite in SA.StatueImages)
-            {
-                if (sprite.name == "ContestedStatue")
-                {
-                    statuea.GetComponent<Image>().sprite = sprite;
-                }
-            }
-
-            var tempColor = statuecapa.GetComponent<Image>().color;
-            tempColor.a = 0f;
-            statuecapa.GetComponent<Image>().color = tempColor;
-        }
-
-        if (team == "Cavemen")
-        {
-            var a = UR.Statues["StatueA"];
-            var statuea = a.transform.Find("StatueStatus");
-            var statuecapa = a.transform.Find("TeamImageCap");
-
-            foreach (Sprite sprite in SA.StatueImages)
-            {
-                if (sprite.name == "CavemanStatue")
-                {
-                    statuea.GetComponent<Image>().sprite = sprite;
-                }
-            }
-
-            foreach (Sprite sprite in TA.CharactersImages)
-            {
-                if (sprite.name == "cavemanleader")
-                {
-                    statuecapa.GetComponent<Image>().sprite = sprite;
-                }
-            }
-
-            var tempColor = statuecapa.GetComponent<Image>().color;
-            tempColor.a = 1f;
-            statuecapa.GetComponent<Image>().color = tempColor;
-        }
-
-        if (team == "Gamers")
-        {
-            var a = UR.Statues["StatueA"];
-            var statuea = a.transform.Find("StatueStatus");
-            var statuecapa = a.transform.Find("TeamImageCap");
-
-            foreach (Sprite sprite in SA.StatueImages)
-            {
-                if (sprite.name == "GamerStatue")
-                {
-                    statuea.GetComponent<Image>().sprite = sprite;
-                }
-            }
-
-            foreach (Sprite sprite in TA.CharactersImages)
-            {
-                if (sprite.name == "gamerleader")
-                {
-                    statuecapa.GetComponent<Image>().sprite = sprite;
-                }
-            }
-
-            var tempColor = statuecapa.GetComponent<Image>().color;
-            tempColor.a = 1f;
-            statuecapa.GetComponent<Image>().color = tempColor;
-        }
-
-        if (team == "Vikings")
-        {
-            var a = UR.Statues["StatueA"];
-            var statuea = a.transform.Find("StatueStatus");
-            var statuecapa = a.transform.Find("TeamImageCap");
-
-            foreach (Sprite sprite in SA.StatueImages)
-            {
-                if (sprite.name == "VikingStatue")
-                {
-                    statuea.GetComponent<Image>().sprite = sprite;
-                }
-            }
-
-            foreach (Sprite sprite in TA.CharactersImages)
-            {
-                if (sprite.name == "vikingleader")
-                {
-                    statuecapa.GetComponent<Image>().sprite = sprite;
-                }
-            }
-
-            var tempColor = statuecapa.GetComponent<Image>().color;
-            tempColor.a = 1f;
-            statuecapa.GetComponent<Image>().color = tempColor;
-        }
-
-        if (team == "Knights")
-        {
-            var a = UR.Statues["StatueA"];
-            var statuea = a.transform.Find("StatueStatus");
-            var statuecapa = a.transform.Find("TeamImageCap");
-
-            foreach (Sprite sprite in SA.StatueImages)
-            {
-                if (sprite.name == "KnightStatue")
-                {
-                    statuea.GetComponent<Image>().sprite = sprite;
-                }
-            }
-
-            foreach (Sprite sprite in TA.CharactersImages)
-            {
-                if (sprite.name == "knightleader")
-                {
-                    statuecapa.GetComponent<Image>().sprite = sprite;
-                }
-            }
-
-            var tempColor = statuecapa.GetComponent<Image>().color;
-            tempColor.a = 1f;
-            statuecapa.GetComponent<Image>().color = tempColor;
-        }
-
-        if (team == "Romans")
-        {
-            var a = UR.Statues["StatueA"];
-            var statuea = a.transform.Find("StatueStatus");
-            var statuecapa = a.transform.Find("TeamImageCap");
-
-            foreach (Sprite sprite in SA.StatueImages)
-            {
-                if (sprite.name == "RomanStatue")
-                {
-                    statuea.GetComponent<Image>().sprite = sprite;
-                }
-            }
-
-            foreach (Sprite sprite in TA.CharactersImages)
-            {
-                if (sprite.name == "romanleader")
-                {
-                    statuecapa.GetComponent<Image>().sprite = sprite;
-                }
-            }
-
-            var tempColor = statuecapa.GetComponent<Image>().color;
-            tempColor.a = 1f;
-            statuecapa.GetComponent<Image>().color = tempColor;
-        }
+        
     }
 
     public void SetStatueLogo()
